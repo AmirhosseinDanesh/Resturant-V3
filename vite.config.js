51fde7bb721dd4d6f564349533392a2555a97ac8
Amir Danesh <62671734+AmirhosseinDanesh@users.noreply.github.com>
2023-07-28 19:32:12 +0330

    fix deploy bug

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://demo.bestwebneed.ir/',
-  build: {
-    outDir: './dist' //Added
-  }
+  // base: 'https://demo.bestwebneed.ir/',
+  // build: {
+  //   outDir: './dist' //Added
+  // }
 })
